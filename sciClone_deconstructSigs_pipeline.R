e4012b8b5aa5a2dcf25b97f2266e829f004eca04
Cait <cait.harrigan@mail.utoronto.ca>
2019-02-20 01:16:33 -0500

    sciClone called

--- sciClone_deconstructSigs_pipeline.R
+++ sciClone_deconstructSigs_pipeline.R
@@ -3,27 +3,25 @@
 # sciClone + DeconstructSigs pipeline
 
 library(sciClone)
+library(tidyr)
 library(deconstructSigs)
 
 setwd("~/Desktop/TrackSig_simulations/")
 
 # get simulation names
-simNames <- list.files("data/mut_types/")
-simNames <- strsplit(simNames, ".mut_types.txt")
+simNames <- list.files("data")
+sel <- grep(x = simNames, "^Simulation")
+simNames <- simNames[sel]
 
 for (simName in simNames){ #for each simulation
 
   # read in vaf data
-  vafTable <- read.table(sprintf("data/mut_types/%s.mut_types.txt", simName))
+  vafTable <- read.table(sprintf("data/%s/%s_sciclone.txt", simName, simName),
+                         stringsAsFactors = F, header = T)
 
   # format for sciClone input
-  vafTable <- vafTable[,c(1,2,4,5,3)]
-  colnames(vafTable) <- c("chr", "pos", "ref_reads", "var_reads", "vaf")
-
-  # simulate read counts
-
-  # scale vaf by 100
-  vafTable$vaf <- apply(vafTable[5], MARGIN = 1, prod, 100)
+  vafTable <- separate(vafTable, chr, into = c("V1", "chr"), sep = "chr")
+  vafTable <- vafTable[,c(2:6)]
 
   # read in CNA data
   cnaTable <- read.table(sprintf("data/%s/%s_cna.txt", simName, simName), header = T)
