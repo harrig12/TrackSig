08760efc8d881c663775ddecc50ed307d53054ff
Cait <cait.harrigan@mail.utoronto.ca>
2019-03-06 14:14:56 -0500

    bin_size tests

--- sim_pipeline.R
+++ sim_pipeline.R
@@ -42,6 +42,9 @@ simulations <- list.files(outdir)
 sel <- grep(x = simulations, "^Simulation")
 simulations <- simulations[sel]
 
+# simulations by bin size
+bin_sizes <- c(30, 75, 100, 156, 213, 250, 300)
+
 # TrackSig - set options (same variables as in in header.R)
 TrackSig.options(purity_file = sim_purity_file,
                  signature_file = "annotation/sigProfiler_SBS_signatures.txt",
@@ -52,7 +55,8 @@ TrackSig.options(purity_file = sim_purity_file,
                  compute_bootstrap = FALSE,
                  cancer_type_signatures = FALSE,
                  pcawg_format = TRUE,
-                 DIR_RESULTS = tracksig_results_dir)
+                 DIR_RESULTS = tracksig_results_dir,
+                 bin_size = 30)
 
 
 # tracksig - make counts
@@ -72,13 +76,13 @@ extract_exposures_per_mutation(activities_dir = paste0(tracksig_results_dir, "/S
 # tracksig - bootstrap not functional yet
 #compute_errorbars_for_all_examples()
 
-res <- compare_simulation_results(simulations, 
-    ground_truth_dir = outdir, 
+res <- compare_simulation_results(simulations,
+    ground_truth_dir = outdir,
     method_results_dir = paste0(tracksig_results_dir, "/SIMULATED/"),
     res_file_name = "TrackSig_simulation_results.txt")
 
 pdf("TrackSig_KL.pdf", width = 5, height=5)
-plot(res$kl, res$abs_diff_max, main="TrackSig KL", 
+plot(res$kl, res$abs_diff_max, main="TrackSig KL",
    xlab="KL", ylab="max abs diff")
 dev.off()
 
