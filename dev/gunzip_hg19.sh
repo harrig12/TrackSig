20685f05c6e3b1cc06581b8699d462a301a21960
Cait <cait.harrigan@mail.utoronto.ca>
2019-05-18 14:39:07 -0700

    folder cleanup

--- dev/gunzip_hg19.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#gunzip hg19 tarballs for TrackSig
-for i in `ls ./annotation/hg19/* | grep "chr\d\(\d\)\?.fa.gz"`;
-do 
-	gunzip $i 
-done
-gunzip ./annotation/hg19/chrX.fa
-gunzip ./annotation/hg19/chrY.fa
-
