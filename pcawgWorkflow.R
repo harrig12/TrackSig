451de2213f7cb804b1e10d808422e5ce7d65a120
Cait Harrigan <cait.harrigan@mail.utoronto.ca>
2019-06-25 12:55:13 -0400

    6.3 remodel load and score for replaced pl funcitonality, and create sim-specific load and score

--- pcawgWorkflow.R
+++ pcawgWorkflow.R
@@ -36,16 +36,17 @@ list <- structure(NA,class="result")
 
 #countsDir <- "~/Desktop/pcawg/data/counts"
 
-loadAndScoreIt_pcawg <- function(samplename, countsDir = "~/Desktop/pcawg/data/counts", tumortypes) {
+loadAndScoreIt_pcawg <- function(vcfFile, tumortypes, acronym) {
 
 
   # load_sample() returns (inorder)
   # list(vcfData, vcf, phis, quadratic_phis, phis_sliding_window, assigns_phylo_nodes,
   #      assigns_phylo_nodes_sw, acronym, window, tumor_id, phis_for_plot, bootstrap_vcfs, bootstrap_phis))
 
-  list[vcfData, vcf, phis, quadratic_phis, phis_sliding_window, assigns_phylo_nodes,
-       assigns_phylo_nodes_sw, acronym, window, tumor_id, phis_for_plot,
-       bootstrap_vcfs, bootstrap_phis] <- load_sample(samplename, countsDir, bootstrapDir = NULL, tumortypes)
+  vcfFile <- path.expand(vcfFile)
+  tumor_id <- strsplit( unlist(strsplit(vcfFile, "/"))[ length( strsplit(vcfFile, "/")[[1]] ) ] , ".vcf")[[1]]
+
+  list[phis, quadPhis, counts] <- vcfToCounts(vcfFile, refGenome = BSgenome.Hsapiens.UCSC.hg19)
 
   # following checking is all from within compute_signatures_for_all_examples()
   # will throw a next error if check fails
@@ -72,7 +73,7 @@ loadAndScoreIt_pcawg <- function(samplename, countsDir = "~/Desktop/pcawg/data/c
     next
   }
 
-  if (sum(vcf[apply(alex.t,1,sum) == 0,] != 0) != 0) {
+  if (sum(counts[apply(alex.t,1,sum) == 0,] != 0) != 0) {
     print(paste0("Sample ", example, ": some trinucleotides have probability 0 under the model, but their count is non-zero. Sot the count vector is impossible under the model."))
     next
   }
@@ -80,22 +81,16 @@ loadAndScoreIt_pcawg <- function(samplename, countsDir = "~/Desktop/pcawg/data/c
   # this is the work-horse part
 
   dir_name <- paste0(TrackSig.options()$DIR_RESULTS, acronym, "/", tumor_id, "/")
-
   suppressWarnings(dir.create(dir_name, recursive = T))
 
-  if (!is.null(phis_for_plot))
-  {
-    write(phis_for_plot, file=paste0(dir_name, "phis.txt"), ncolumns=length(phis_for_plot))
-  }
-
-  method_name <- "iterativeChangePoints"
+  #method_name <- "iterativeChangePoints"
 
   if (!file.exists(paste0(dir_name, "mixtures.csv")) || !file.exists(paste0(dir_name, "changepoints.txt")))
   {
     if (TrackSig.options()$changepoint_method == "PELT") {
-      list[changepoints, mixtures] <- TrackSig:::find_changepoints_pelt(vcf, alex.t, phis, quadratic_phis)
+      list[changepoints, mixtures] <- TrackSig:::find_changepoints_pelt(counts, alex.t, phis, quadPhis)
     } else {
-      list[bics, optimal, changepoints, mixtures] <- find_changepoints_over_all_signatures_one_by_one(vcf, alex.t, n_signatures = ncol(alex.t))
+      list[bics, optimal, changepoints, mixtures] <- find_changepoints_over_all_signatures_one_by_one(counts, alex.t, n_signatures = ncol(alex.t))
     }
 
     write.csv(mixtures, file=paste0(dir_name, "mixtures.csv"))
@@ -112,28 +107,109 @@ loadAndScoreIt_pcawg <- function(samplename, countsDir = "~/Desktop/pcawg/data/c
     }
   }
 
-  if (!is.null(assigns_phylo_nodes_sw)) {
-    write(assigns_phylo_nodes_sw,  file=paste0(dir_name, "assignments.txt"), ncolumns=length(assigns_phylo_nodes_sw))
-  } else  {
-    n_clusters = transition_points = assigns_phylo_nodes_sw = NULL
-  }
 
   plot_name <- paste0(dir_name, "/", acronym, "_", tumor_id, "_", TrackSig.options()$sig_amount, TrackSig.options()$postfix, ".pdf")
 
-  if (TrackSig.options()$PLOT_FULL_NAME)
-  {
-    plot_name <- paste0(dir_name, "/", acronym, "_", data_method, "_multMix_fittedPerTimeSlice_", sig_amount, "_noPrior_", method_name, postfix, ".pdf")
-  }
+  #if (TrackSig.options()$PLOT_FULL_NAME)
+  #{
+  #  plot_name <- paste0(dir_name, "/", acronym, "_", data_method, "_multMix_fittedPerTimeSlice_", sig_amount, "_noPrior_", method_name, postfix, ".pdf")
+  #}
 
   mark_cp <- !is.null(changepoints)
-  TrackSig:::plot_signatures(mixtures*100, plot_name=plot_name, phis = phis_for_plot, mark_change_points=mark_cp, change_points=changepoints,
-                  #assigns_phylo_nodes = assigns_phylo_nodes_sw,
-                  transition_points = transition_points,
+  TrackSig:::plot_signatures(mixtures*100, plot_name=plot_name, phis = phis, mark_change_points=mark_cp, change_points=changepoints,
+                  transition_points = NULL,
                   scale=1.2)
 
   mixtures.rescaled = NULL
 }
 
+loadAndScoreIt_simulation <- function(vcfFile, countsDir, tumortypes, acronym = "SIMUlATION") {
+
+
+  # load_sample() returns (inorder)
+  # list(vcfData, vcf, phis, quadratic_phis, phis_sliding_window, assigns_phylo_nodes,
+  #      assigns_phylo_nodes_sw, acronym, window, tumor_id, phis_for_plot, bootstrap_vcfs, bootstrap_phis))
+
+  vcfFile <- path.expand(vcfFile)
+  tumor_id <- strsplit( unlist(strsplit(vcfFile, "/"))[ length( strsplit(vcfFile, "/")[[1]] ) ] , ".vcf")[[1]]
+
+  # list[phis, quadPhis, counts] <- vcfToCounts(vcfFile, refGenome = BSgenome.Hsapiens.UCSC.hg19)
+  list[phis, quadPhis, counts] <- load_sample(tumor_id, countsDir, NULL, tumortypes)[c(3, 4, 2)]
+
+  # following checking is all from within compute_signatures_for_all_examples()
+  # will throw a next error if check fails
+
+  if (TrackSig.options()$sig_amount == "onlyKnownSignatures") {
+    # Fit only known signatures
+    list[alex.t, matched_type, acronym] <- TrackSig:::get_signatures_for_current_sample(tumor_id, active_signatures.our_samples, alex, TrackSig.options()$noise_sig)
+  } else {
+    alex.t <- alex
+  }
+
+  if (is.null(acronym) || acronym == "") {
+    print(paste("ERROR: Cancer type not found for ", example))
+    next
+  }
+
+  if (is.null(alex.t))
+  {
+    print(paste0("No active signatures for sample", example, " ...."))
+    next
+  }
+
+  if (is.vector(alex.t)) {
+    next
+  }
+
+  if (sum(counts[apply(alex.t,1,sum) == 0,] != 0) != 0) {
+    print(paste0("Sample ", example, ": some trinucleotides have probability 0 under the model, but their count is non-zero. Sot the count vector is impossible under the model."))
+    next
+  }
+
+  # this is the work-horse part
+
+  dir_name <- paste0(TrackSig.options()$DIR_RESULTS, acronym, "/", tumor_id, "/")
+  suppressWarnings(dir.create(dir_name, recursive = T))
+
+  #method_name <- "iterativeChangePoints"
+
+  if (!file.exists(paste0(dir_name, "mixtures.csv")) || !file.exists(paste0(dir_name, "changepoints.txt")))
+  {
+    if (TrackSig.options()$changepoint_method == "PELT") {
+      list[changepoints, mixtures] <- TrackSig:::find_changepoints_pelt(counts, alex.t, phis, quadPhis)
+    } else {
+      list[bics, optimal, changepoints, mixtures] <- find_changepoints_over_all_signatures_one_by_one(counts, alex.t, n_signatures = ncol(alex.t))
+    }
+
+    write.csv(mixtures, file=paste0(dir_name, "mixtures.csv"))
+
+    n_col <- ifelse(length(changepoints) > 0, length(changepoints), 1)
+    write(changepoints, file=paste0(dir_name, "changepoints.txt"), ncolumns=n_col)
+  } else {
+    mixtures <- read_mixtures(paste0(dir_name, "mixtures.csv"))
+    cp_file = paste0(dir_name, "changepoints.txt")
+    if (file.info(cp_file)$size == 1) {
+      changepoints <- c()
+    } else {
+      changepoints <- unlist(read.table(cp_file, header=F))
+    }
+  }
+
+
+  plot_name <- paste0(dir_name, "/", acronym, "_", tumor_id, "_", TrackSig.options()$sig_amount, TrackSig.options()$postfix, ".pdf")
+
+  #if (TrackSig.options()$PLOT_FULL_NAME)
+  #{
+  #  plot_name <- paste0(dir_name, "/", acronym, "_", data_method, "_multMix_fittedPerTimeSlice_", sig_amount, "_noPrior_", method_name, postfix, ".pdf")
+  #}
+
+  mark_cp <- !is.null(changepoints)
+  TrackSig:::plot_signatures(mixtures*100, plot_name=plot_name, phis = phis, mark_change_points=mark_cp, change_points=changepoints,
+                             transition_points = NULL,
+                             scale=1.2)
+
+  mixtures.rescaled = NULL
+}
 
 
 
@@ -149,10 +225,12 @@ loadAndScoreIt_pcawg <- function(samplename, countsDir = "~/Desktop/pcawg/data/c
 
 
 
-
+##################################################
 # SIMULATION WORKFLOW
+#################################################
 
 library(TrackSig)
+library(BSgenome.Hsapiens.UCSC.hg19)
 library(foreach)
 library(doParallel)
 
@@ -186,14 +264,17 @@ simnames <- list.files("~/Desktop/pcawg/simulations_data/")
 sel <- grepl("100$", simnames)
 simnames <- simnames[sel]
 
-foreach (i=1:length(simnames)) %dopar% {
-  simname = simnames[i]
-  run_simulation(simname, "simulations_data")
-  loadAndScoreIt_pcawg(simname, "~/Desktop/pcawg/simulations_data/counts/", tumortypes)
-}
+#foreach (i=1:length(simnames)) %dopar% {
+#  simname = simnames[i]
+#  run_simulation(simname, "simulations_data")
+#  loadAndScoreIt_pcawg(simname, "~/Desktop/pcawg/simulations_data/counts/", tumortypes)
+#}
 
-#run_simulation(simnames[5], "simulations_data")
-#loadAndScoreIt_pcawg(simnames[5], "~/Desktop/pcawg/simulations_data/counts/", tumortypes)
+loadAndScoreIt_simulation(vcfFile = paste0("~/Desktop/pcawg/simulations_data/", simnames[1], "/", simnames[1], ".vcf"),
+                          countsDir = "~/Desktop/pcawg/simulations_data/counts/", tumortypes = tumortypes)
+
+#run_simulation(simnames[1], "simulations_data")
+#loadAndScoreIt_pcawg(simnames[1], "~/Desktop/pcawg/simulations_data/counts/", tumortypes)
 
 
 ## compare cp's found
@@ -224,35 +305,40 @@ for (sim_dir in list.files("~/Desktop/pcawg/simulation_results/")){
   sim_cps[[sim_dir]] <- cps
 }
 
-sims <- c("Simulation_two_clusters1_depth100_bin100",
-"Simulation_two_clusters1_depth100_bin50",
-"Simulation_two_clusters1_depth100_bin80",
-"Simulation_two_clusters10_depth100_bin100",
-"Simulation_two_clusters10_depth100_bin50",
-"Simulation_two_clusters10_depth100_bin80",
-"Simulation_two_clusters2_depth100_bin100",
-"Simulation_two_clusters2_depth100_bin50",
-"Simulation_two_clusters2_depth100_bin80",
-"Simulation_two_clusters3_depth100_bin100",
-"Simulation_two_clusters3_depth100_bin50",
-"Simulation_two_clusters3_depth100_bin80",
-"Simulation_two_clusters4_depth100_bin100",
-"Simulation_two_clusters4_depth100_bin50",
-"Simulation_two_clusters4_depth100_bin80",
-"Simulation_two_clusters5_depth100_bin100",
-"Simulation_two_clusters5_depth100_bin50",
-"Simulation_two_clusters5_depth100_bin80",
-"Simulation_two_clusters6_depth100_bin100",
-"Simulation_two_clusters6_depth100_bin50",
-"Simulation_two_clusters6_depth100_bin80",
-"Simulation_two_clusters7_depth100_bin100",
-"Simulation_two_clusters7_depth100_bin50",
-"Simulation_two_clusters7_depth100_bin80",
-"Simulation_two_clusters8_depth100_bin100",
-"Simulation_two_clusters8_depth100_bin50",
-"Simulation_two_clusters8_depth100_bin80",
-"Simulation_two_clusters9_depth100_bin100",
-"Simulation_two_clusters9_depth100_bin50",
-"Simulation_two_clusters9_depth100_bin80")
+#vcfFile <- "~/Desktop/pcawg/data/Simulation_two_clusters4_depth100_bin100/Simulation_two_clusters4_depth100_bin100.vcf"
+
+##################################################
+# EXAMPLE.vcf
+#################################################
+
+
+# set up
+
+library(TrackSig)
+library(BSgenome.Hsapiens.UCSC.hg19)
+
+TrackSig.options(purity_file = system.file("extdata", "example_purity.txt", package = "TrackSig"),
+                 signature_file = "~/Desktop/pcawg/annotation/sigProfiler_SBS_signatures.txt",
+                 #trinucleotide_file = "~/Desktop/pcawg/annotation/trinucleotide.txt",
+                 #active_signatures_file = "~/Desktop/pcawg/annotation/sim_active_in_sample.txt",
+                 tumortype_file = system.file("extdata", "tumortypes.txt", package = "TrackSig"),
+                 sig_amount = "onlyKnownSignatures",
+                 compute_bootstrap = FALSE,
+                 cancer_type_signatures = FALSE,
+                 pcawg_format = TRUE,
+                 DIR_RESULTS = "simulation_results/",
+                 pelt_penalty = expression( (n_sigs - 1) * log(n_bins) + log(n_bins) ),
+                 pelt_score_fxn = TrackSig:::sum_gaussian_mixture_multinomials_ll,
+                 bin_size = 100)
+
+load("~/Desktop/pcawg/annotation/pcawg_annotation.Rdata")
+active_signatures.our_samples$ID[1] <- "example"
+tumortypes <- data.frame(ID = "example", tumor_type = "SIMULATED")
+
+# use pcawg funtion
+
+vcfFile <- system.file("extdata", "example.vcf", package = "TrackSig")
+loadAndScoreIt_pcawg(vcfFile, tumortypes = tumortypes, acronym = "SIMULATED")
+
 
 # [END]
