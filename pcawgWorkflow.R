497f8785dfb8f81e24b27a7494978da70dfa8806
Cait Harrigan <cait.harrigan@mail.utoronto.ca>
2019-06-27 15:50:44 -0400

    sim comparison (cp)

--- pcawgWorkflow.R
+++ pcawgWorkflow.R
@@ -4,6 +4,22 @@
 # Author: Cait Harrigan
 # June 2019
 
+# SETUP ##############################################################
+
+reticulate::use_condaenv("tracksig")
+library(TrackSig)
+library(BSgenome.Hsapiens.UCSC.hg19.masked)
+library(foreach)
+library(doParallel)
+
+# Remember: unless registerDoMC is called, foreach will not run in parallel. Simply loading the doParallel package is not enough
+registerDoParallel(cores=20)
+
+setwd("~/Desktop/pcawg/")
+TrackSig:::create_simulation_set(outdir = "~/Desktop/pcawg/simulation_data")
+
+
+# DEFS ##############################################################
 
 # conveinent list stuct
 list <- structure(NA,class="result")
@@ -117,18 +133,6 @@ loadAndScoreIt_pcawg <- function(vcfFile, tumortypes, acronym,
 
 # SIMULATION WORKFLOW ##################################################
 
-reticulate::use_condaenv("tracksig")
-library(TrackSig)
-library(BSgenome.Hsapiens.UCSC.hg19.masked)
-library(foreach)
-library(doParallel)
-
-
-# Remember: unless registerDoMC is called, foreach will not run in parallel. Simply loading the doParallel package is not enough
-registerDoParallel(cores=20)
-
-setwd("~/Desktop/pcawg/")
-# TrackSig:::create_simulation_set(outdir = "~/Desktop/pcawg/simulation_data")
 
 # set up
 TrackSig.options(purity_file = "~/Desktop/pcawg/annotation/sim_purity.txt",
@@ -167,6 +171,67 @@ foreach (i=1:length(simnames)) %dopar% {
 
 }
 
+# SIMULATION COMPARISON #############################
+
+# want to compare quality of simulaitons.
+# need ground truth information, as well as scoring for each simulation.
+
+getSimMeta <- function(dataDir){
+  # pickup simulation info
+
+  simUnparsed <- list.files(dataDir)
+  simMeta <-data.frame(row.names = simUnparsed)
+
+  simUnparsed <- strsplit(simUnparsed, "_")
+  simUnparsed <- t(as.data.frame(simUnparsed, stringsAsFactors = F))
+  rownames(simUnparsed) <- NULL
+  simUnparsed[,1] <- paste0(simUnparsed[,1], simUnparsed[,2], simUnparsed[,3])
+  simUnparsed <- simUnparsed[,c(1, 4:7)]
+  colnames(simUnparsed) <- c("type", "depth", "bin", "dist", "sigChange")
+  simUnparsed <- data.frame(simUnparsed, stringsAsFactors = F)
+
+  # populate the meta df
+  simMeta$type <- simUnparsed$type
+  simMeta$depth <- as.numeric(unlist(strsplit(simUnparsed$depth, "depth"))[c(F,T)])
+  simMeta$bin <- as.numeric(unlist(strsplit(simUnparsed$bin, "bin"))[c(F,T)])
+  simMeta$dist <- as.numeric(unlist(strsplit(simUnparsed$dist, "dist"))[c(F,T)])
+  simMeta$sigChange <- as.numeric(unlist(strsplit(simUnparsed$sigChange, "sigChange"))[c(F,T)])
+
+  return(simMeta)
+}
+
+getSimCPs <- function(resultDir, simMeta){
+  # pick up the found changepoints and their associated phi
+
+  #simNames <- rownames(simMeta)
+  simNames <- list.files(resultDir)
+  CPs <- list()
+
+  for (simName in simNames) {
+
+    # try to read cps, handle if file empty
+    simCPs <- tryCatch({read.table(paste0(resultDir, "/", simName, "/changepoints.txt"))},
+                    error = function(err){ return("*") })
+
+    CPs[[simName]] <- simCPs
+  }
+
+  return(CPs)
+}
+
+
+simMeta <- getSimMeta("~/Desktop/sigAddSimulation_results/")
+
+# ground truth cps's placed exactly between 1.0 and dist. Only fair if
+# variance is equal.
+gtCPs <- (simMeta$dist + 1) / 2
+
+simCPs <- getSimCPs("~/Desktop/sigAddSimulation_results/", simMeta)
+
+correct <- lapply(simCPs, length) == 1
+names(correct) <- NULL
+
+simMeta$correctNcp <- correct
 
 
 
